6f96f75d84dc6d1b3432e671f771839317c391c2
Juanesb230 <juanesb230@gmail.com>
2020-05-30 14:12:12 -0500

    package commit

--- NativePrueba/App.js
+++ NativePrueba/App.js
@@ -5,6 +5,7 @@ import { store, persistor } from './src/store';
 import {PersistGate} from 'redux-persist/es/integration/react'
 
 export default class App extends React.Component {
+  
   render() {
     return(
       <Provider store={store}>
